e08b021a0869eff23a10eb7bc84bd41a9b92a76e
zhenjie zhu <devops@kubectl.net>
2024-05-13 21:11:00 +0800

    Add Docker configuration files for Netty project
    This commit includes the addition of Docker configuration files for the Netty project. It includes a Dockerfile which sets up the required environment, the build script to automate the process, and a Docker Compose file to manage the service. All these contribute to the seamless deployment of the Netty application in a Dockerized environment.

---
+++ netty/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# shellcheck disable=SC2164
+SHELL_FOLDER=$(cd "$(dirname "$0")" && pwd)
+cd "$SHELL_FOLDER"
+
+# shellcheck disable=SC1090
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
+
+cd ../
+log "build" "step1 build jar"
+bash <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/gradle/build.sh) \
+  -c "gradle-8.4_cache" \
+  -i "gradle:8.4" \
+  -x "netty:clean netty:build -x test"
+
+jar_name="socks5.jar"
+
+cd netty/
+
+if [ ! -f "build/libs/$jar_name" ]; then
+  log "verify" "gradle build failed"
+  exit 1
+fi
+
+log "build" "step2 build & push image"
+image_name="registry.cn-shanghai.aliyuncs.com/iproute/netty-socks5"
+
+bash <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/docker/build.sh) \
+  -i "$image_name" \
+  -v "latest" \
+  -r "false" \
+  -p "true"
