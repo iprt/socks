01d5e791ccae38e67591d2c33f6aeeee2bb38815
zhenjie zhu <devops@kubectl.net>
2024-05-14 02:03:40 +0800

    Add configuration file support to HexDumpProxy
    This commit introduces a configuration file (config.properties) to the HexDumpProxy class. It first attempts to read properties from this configuration file. If the file does not exist, the system properties would be used as default instead.

--- netty-tcp-proxy/src/main/java/org/iproute/HexDumpProxy.java
+++ netty-tcp-proxy/src/main/java/org/iproute/HexDumpProxy.java
@@ -7,7 +7,12 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.iproute.handler.HexDumpProxyInitializer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 /**
  * HexDumpProxy
@@ -15,14 +20,18 @@ import org.iproute.handler.HexDumpProxyInitializer;
  * @author winterfell
  * @since 2022/7/10
  */
+@Slf4j
 public final class HexDumpProxy {
 
-    static final String LOCAL_HOST = System.getProperty("localHost", "127.0.0.1");
-    static final int LOCAL_PORT = Integer.parseInt(System.getProperty("localPort", "3306"));
-    static final String REMOTE_HOST = System.getProperty("remoteHost", "172.100.1.100");
-    static final int REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "3306"));
+    static String LOCAL_HOST;
+    static int LOCAL_PORT;
+    static String REMOTE_HOST;
+    static int REMOTE_PORT;
 
     public static void main(String[] args) throws Exception {
+
+        readProperties();
+
         // InternalLoggerFactory.setDefaultFactory(new Slf4JLoggerFactory());
         System.err.println("Proxying *:" + LOCAL_PORT + " to " + REMOTE_HOST + ':' + REMOTE_PORT + " ...");
 
@@ -43,6 +52,25 @@ public final class HexDumpProxy {
             workerGroup.shutdownGracefully();
         }
 
+    }
+
+    static void readProperties() throws Exception {
+        if (new File("config.properties").exists()) {
+            log.info("read config file");
+
+            Properties p = new Properties();
+            p.load(new FileInputStream("config.properties"));
+            LOCAL_HOST = p.getProperty("localHost", "127.0.0.1");
+            LOCAL_PORT = Integer.parseInt(p.getProperty("localPort", "3306"));
+            REMOTE_HOST = p.getProperty("remoteHost", "172.100.1.100");
+            REMOTE_PORT = Integer.parseInt(p.getProperty("remotePort", "3306"));
+
+        } else {
+            LOCAL_HOST = System.getProperty("localHost", "127.0.0.1");
+            LOCAL_PORT = Integer.parseInt(System.getProperty("localPort", "3306"));
+            REMOTE_HOST = System.getProperty("remoteHost", "172.100.1.100");
+            REMOTE_PORT = Integer.parseInt(System.getProperty("remotePort", "3306"));
+        }
     }
 
 }
